a0ac2de5d1eaf5189b03ee809523bf39db460ea1
Nimra Ahmed <50912134+nimraahmed@users.noreply.github.com>
2020-08-17 13:42:40 +0500

    JS file for About Us page

---
+++ about-us.js
@@ -0,0 +1,31 @@
+
+const hamburger=document.querySelector('.hamburger');
+const navlinks=document.querySelector('.nav-links')
+hamburger.addEventListener('click',()=>{
+    navlinks.classList.toggle('open');
+})
+
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("nav").style.top = "0";
+  } else {
+    document.querySelector("nav").style.top = "-12vh";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+const firstp=document.querySelector("#first");
+window.addEventListener('scroll',function(){
+    let offset=window.pageYOffset;
+    console.log(firstp)
+    firstp.style.backgroundPositionY=offset*0.7+"px";
+    console.log(offset)
+    
+})
+
+
