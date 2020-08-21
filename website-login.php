0de9414047863e91e5246ccdc419db9e29552f20
Nimra Ahmed <50912134+nimraahmed@users.noreply.github.com>
2020-08-21 20:04:50 +0500

    Update website-login.php

--- website-login.php
+++ website-login.php
@@ -52,6 +52,7 @@ if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['login']
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="website-login.css">
+      <link href="https://fonts.googleapis.com/css2?family=Alata&family=Libre+Baskerville:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta charset="utf-8">
