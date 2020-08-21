8ec6863e950bdab98d027f05dd39bf9e2a21bd44
najiagul <najiagul.2000@gmail.com>
2020-08-22 00:53:53 +0500

    changed css files names

--- website-signup.php
+++ website-signup.php
@@ -48,7 +48,7 @@ isset($_POST['password']) && isset($_POST['cpassword']) )
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=yes" >
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="website-signup.css"> 
+    <link rel="stylesheet" href="website_signup.css"> 
      <link href="https://fonts.googleapis.com/css2?family=Alata&family=Libre+Baskerville:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
